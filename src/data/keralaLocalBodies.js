869e8ebe65c6cd0d2df4e907e86327d9e147d716
amsadams <anandu.email@gmail.com>
2025-08-11 05:16:34 +0530

    Kerala P,M,C datas are added

---
+++ src/data/keralaLocalBodies.js
@@ -0,0 +1,236 @@
+// Kerala Local Bodies Data Structure
+// Based on the Excel file data provided
+
+export const KERALA_LOCAL_BODIES = {
+  "Alappuzha": {
+    "panchayaths": [
+      "Ala", "Ambalapuzha North", "Ambalapuzha South", "Arattupuzha", "Arookutty", 
+      "Aroor", "Aryad", "Bharanickavu", "Budhanoor", "Champakulam", 
+      "Chennam Pallippuram", "Chennithala-thriperumthura", "Cheppad", "Cheriyanad", 
+      "Cherthala South", "Cheruthana", "Chettikulangura", "Chingoli", "Chunakkara", 
+      "Devikulangara", "Eduthua", "Ezhupunna", "Kadakkarappally", "Kainakary", 
+      "Kandaloor", "Kanjikuzhi", "Karthikapally", "Karuvatta", "Kavalam", 
+      "Kodamthuruth", "Krishnapuram", "Kumarapuram", "Kuthiyathodu", "Mannancherry", 
+      "Mannar", "Mararikulam North", "Mararikkulam South", "Muhamma", "Mulakkuzha", 
+      "Muthukulam", "Muttar", "Nedumudi", "Neelamperoor", "Nooranad", "Palamel", 
+      "Pallippad", "Panavally", "Pandanad", "Pathiyoor", "Pattanakkad", 
+      "Perumbalam", "Pulikunnu", "Pulyoor", "Punnapra north", "Punnapra south", 
+      "Purakkadu", "Ramankary", "Thakazhi", "Thalavadi", "Thamarakkulam", 
+      "Thanneermukkam", "Thazhakkara", "Thekkekara", "Thikkattussery", 
+      "Thiruvanvandoor", "Thrikkunnapuzha", "Thuravoor", "Vallikunnam", 
+      "Vayalar", "Veeyapuram", "Veliyanad", "Venmoney"
+    ],
+    "municipalities": [
+      "Alappuzha", "Chengannur", "Cherthala", "Haripad", "Kayamkulam", "Mavelikkara"
+    ],
+    "corporations": []
+  },
+  
+  "Ernakulam": {
+    "panchayaths": [
+      "Aikkaranadu", "Alangad", "Amballoor", "Arakuzha", "Asamannoor", "Avoly", 
+      "Ayavana", "Ayyampuzha", "Chellanam", "Chendamangalam", "Chengamanad", 
+      "Cheranallur", "Chittattukara", "Choornikkara", "Chottanikkara", 
+      "Edakkattuvayal", "Edathala", "Edavanakkad", "Elamkunnapuzha", "Elanji", 
+      "Ezhikkara", "Kadamakkudy", "Kadungallur", "Kalady", "Kalloorkad", 
+      "Kanjoor", "Karimalloor", "Karukutty", "Kavalangad", "Keerampara", 
+      "Keezhmad", "Kizhakkambalam", "Koovappady", "Kottapady", "Kottuvally", 
+      "Kumbalam", "Kumbalangy", "Kunnathunadu", "Kunnukara", "Kuttampuzha", 
+      "Kuzhuppilly", "Malayattoor", "Maneed", "Manjalloor", "Manjapra", 
+      "Marady", "Mazhuvannoor", "Mookkannoor", "Mudakkuzha", "Mulamthuruthy", 
+      "Mulavukad", "Nayarambalam", "Nedumbassery", "Nellikuzhy", "Njarackal", 
+      "Okkal", "Paingottoor", "Paipra", "Palakuzha", "Pallarimangalam", 
+      "Pallipuram", "Pampakuda", "Parakkadav", "Pindimana", "Poothrikka", 
+      "Pothanicad", "Puthenvelikkara", "Ramamangalam", "Rayamangalam", 
+      "Sreemulanagaram", "Thirumarady", "Thiruvaniyoor", "Thuravoor", 
+      "Udayamperoor", "Vadakekkara", "Vadavucode-puthencruz", "Valakam", 
+      "Varapetty", "Varapuzha", "Vazhakkulam", "Vengola", "Vengoor"
+    ],
+    "municipalities": [
+      "Aluva", "Angamaly", "Eloor", "Kalamassery", "Koothattukulam", 
+      "Kothamangalam", "Maradu", "Muvattupuzha", "North Paravur", 
+      "Perumbavoor", "Piravom", "Thrikkakkara", "Thrippunithura"
+    ],
+    "corporations": ["Cochin"]
+  },
+  
+  "Thiruvananthapuram": {
+    "panchayaths": [
+      "Anchuthengu", "Aruvikkara", "Athiyanoor", "Balaramapuram", "Chirayinkeezhu", 
+      "Karumkulam", "Kazhakoottam", "Kilimanoor", "Kollayil", "Kottukal", 
+      "Malayinkeezhu", "Mangalapuram", "Maranallur", "Nagaroor", "Navaikulam", 
+      "Nemom", "Ottasekharamangalam", "Pallichal", "Panavoor", "Parasala", 
+      "Perumkadavila", "Pullampara", "Sreekariyam", "Thirupuram", "Thiruvananthapuram", 
+      "Uzhamalackal", "Vamanapuram", "Vellanad", "Venganur", "Vilavancode"
+    ],
+    "municipalities": ["Attingal", "Nedumangad", "Varkala"],
+    "corporations": ["Thiruvananthapuram"]
+  },
+  
+  "Kollam": {
+    "panchayaths": [
+      "Adichanallur", "Alayamon", "Anchal", "Bharanikavu", "Chavara", 
+      "Chirakkara", "Clappana", "Chadayamangalam", "Elamad", "Elampalloor", 
+      "Ittiva", "Kadakkal", "Kaluvathukkal", "Kamukumchery", "Karavalur", 
+      "Karunagappalli", "Kizhakkethara", "Kottarakkara", "Kulakkada", 
+      "Kulathupuzha", "Kunnathur", "Mayyanad", "Melila", "Mynagappally", 
+      "Nedumpana", "Nilamel", "Oachira", "Panayam", "Panmana", "Pathanapuram", 
+      "Perayam", "Perinad", "Piravanthur", "Pooyappally", "Poruvazhy", 
+      "Punalur", "Sasthamcotta", "Sooranad North", "Sooranad South", 
+      "Thalavoor", "Thodiyoor", "Thrikkadavoor", "Thrikkaruva", "Thrikkovilvattom", 
+      "Ummannur", "Vadakkumthala", "Velinallur", "Veliyam", "Vilakkudy", "West Kallada"
+    ],
+    "municipalities": ["Kollam", "Karunagappally", "Paravur", "Punalur"],
+    "corporations": ["Kollam"]
+  },
+  
+  "Kottayam": {
+    "panchayaths": [
+      "Akalakunnam", "Ayarkunnam", "Aymanam", "Bharananganam", "Chempu", 
+      "Chirakkadavu", "Elikulam", "Ettumanur", "Kadanad", "Kadaplamattam", 
+      "Kanakkary", "Kangazha", "Karoor", "Karukachal", "Kidangoor", 
+      "Kooroppada", "Koruthodu", "Kozhuvanal", "Kumarakom", "Kurichi", 
+      "Madappally", "Manarcad", "Manjoor", "Marangattupilly", "Maravanthuruthu", 
+      "Meenachil", "Meenadom", "Melukavu", "Moonnilav", "Mulakkulam", 
+      "Mundakayam", "Murukkumpuzha", "Nedumkunnam", "Neendoor", "Neezhoor", 
+      "Paippad", "Pala", "Pallom", "Panachikkad", "Parathodu", "Poonjar", 
+      "Povathur", "Puthuppally", "Ramapuram", "Teekkoy", "Teekoy", 
+      "Thalappalam", "Thalayazham", "Thalayolaparambu", "Thidanad", 
+      "Thiruvarppu", "Thrikkodithanam", "Tvalikulam", "Uzhavoor", 
+      "Vaikom", "Vakathanam", "Velloor", "Vijayapuram"
+    ],
+    "municipalities": ["Changanassery", "Kottayam", "Pala", "Vaikom"],
+    "corporations": []
+  },
+  
+  "Thrissur": {
+    "panchayaths": [
+      "Adat", "Adatt", "Athirappilly", "Avanur", "Chalakkudy", "Chazhur", 
+      "Chelakkara", "Cherpu", "Chowannur", "Desamangalam", "Eranallur", 
+      "Eriyadu", "Erumapetty", "Kadangode", "Kadavallur", "Kadukutty", 
+      "Kaipamangalam", "Kandanasseri", "Karalam", "Karakulam", "Karukutty", 
+      "Kattur", "Kavassery", "Kodakara", "Kodassery", "Kodungallur", 
+      "Kolazhy", "Koratty", "Kottapady", "Kottussery", "Kuzhur", 
+      "Lakkidi", "Madakkathara", "Mala", "Mathilakam", "Melur", 
+      "Mullassery", "Mundur", "Muriyad", "Nattika", "Nenmanikkara", 
+      "Ollukkara", "Padiyur", "Pananchery", "Paralam", "Pavaratty", 
+      "Peringottukara", "Poomangalam", "Porkkalam", "Poyya", "Pudukad", 
+      "Puzhakkal", "Sreenarayanapuram", "Thalapilly", "Thekkumkara", 
+      "Thriprayar", "Thrissur", "Vallathol Nagar", "Varandarappilly", 
+      "Vellangallur", "Velookkara", "Wadakkanchery"
+    ],
+    "municipalities": ["Chalakkudy", "Guruvayur", "Irinjalakuda", "Kodungallur", "Thrissur"],
+    "corporations": ["Thrissur"]
+  },
+  
+  // Add more districts as needed...
+  "Palakkad": {
+    "panchayaths": [
+      "Agali", "Akathethara", "Alathur", "Anakkara", "Elavanchery", 
+      "Kannambra", "Koppam", "Kuzhalmannam", "Malampuzha", "Mathur", 
+      "Melarcode", "Mundur", "Muthalamada", "Nalleppilly", "Nattukal", 
+      "Nelliayampathy", "Nenmara", "Palakkad", "Paruthur", "Pirayiri", 
+      "Polpully", "Pudunagaram", "Pudussery", "Sholayur", "Sreekrishnapuram", 
+      "Tarur", "Thenkurissi", "Thrithala", "Vadakkencherry", "Vadavanoor", 
+      "Vandazhy", "Vilayur"
+    ],
+    "municipalities": ["Chittur-Thathamangalam", "Mannarkkad", "Ottappalam", "Palakkad", "Shoranur"],
+    "corporations": []
+  },
+  
+  "Malappuram": {
+    "panchayaths": [
+      "Anakkayam", "Areekode", "Athavanad", "Chelembra", "Chemmad", 
+      "Chokkad", "Edakkara", "Edappal", "Edarikode", "Irimbiliyam", 
+      "Kaladi", "Kalikavu", "Karulai", "Keezhattur", "Kizhuparamba", 
+      "Kondotty", "Kuruva", "Makkaraparamba", "Malappuram", "Marakkara", 
+      "Melattur", "Moorkkanad", "Nilambur", "Othukkungal", "Pandikkad", 
+      "Perinthalmanna", "Ponmundam", "Pulamanthole", "Puzhakkattiri", 
+      "Tanalur", "Tanur", "Tenhipalam", "Thirunavaya", "Thiruvali", 
+      "Trikkalangode", "Triprangode", "Vazhayur", "Vazhikkadavu", 
+      "Vengara", "Wandoor"
+    ],
+    "municipalities": ["Manjeri", "Nilambur", "Perinthalmanna", "Ponnani", "Tanur", "Tirur"],
+    "corporations": []
+  },
+  
+  "Kozhikode": {
+    "panchayaths": [
+      "Balussery", "Chelannur", "Chengottukavu", "Cheruvannur", "Edakkad", 
+      "Kakkodi", "Kayanna", "Kodenchery", "Koduvally", "Kunnamangalam", 
+      "Madavur", "Mavoor", "Mukkam", "Narippatta", "Naripatta", 
+      "Olavanna", "Omassery", "Panangad", "Panthalayani", "Payyoli", 
+      "Perambra", "Purameri", "Quilandy", "Thikkodi", "Thuneri", 
+      "Thiruvallur", "Ulliyeri", "Unnikulam", "Vadakara", "Villiappally"
+    ],
+    "municipalities": ["Feroke", "Koyilandy", "Vadakara"],
+    "corporations": ["Kozhikode"]
+  },
+  
+  "Kannur": {
+    "panchayaths": [
+      "Alakode", "Ancharakandy", "Anthoor", "Aralam", "Ayyankunnu", 
+      "Cheruthazham", "Chirakkal", "Chokli", "Dharmadam", "Edakkad", 
+      "Irikkur", "Kadannappalli", "Kalliasseri", "Kannapuram", "Kannur", 
+      "Karikkottakkary", "Koothuparamba", "Maloor", "Mattannur", 
+      "Muzhappilangad", "Narath", "Newmahe", "Pallikkunnu", "Panoor", 
+      "Parassinikkadavu", "Payam", "Peralasseri", "Peravoor", "Pilathara", 
+      "Puzhathi", "Sreekandapuram", "Thaliparamba", "Thalassery", 
+      "Thripangottur", "Ulikkal", "Valapattanam"
+    ],
+    "municipalities": ["Kannur", "Mattannur", "Payyannur", "Thalassery"],
+    "corporations": []
+  },
+  
+  "Kasaragod": {
+    "panchayaths": [
+      "Badiadka", "Badiyadukka", "Balal", "Bedadka", "Bellur", 
+      "Chemnad", "Chengala", "Delampady", "Karadka", "Kasaragod", 
+      "Kumbadaje", "Kumbla", "Madhur", "Manjeshwar", "Mogral Puthur", 
+      "Muliyar", "Nileshwar", "Padanna", "Pallikkara", "Panathur", 
+      "Pilicode", "Trikaripur", "Udma", "Uppala", "Vellarikundu"
+    ],
+    "municipalities": ["Kasaragod"],
+    "corporations": []
+  },
+  
+  "Idukki": {
+    "panchayaths": [
+      "Adimali", "Alacode", "Arakulam", "Ayyappancovil", "Bisonvalley", 
+      "Chakkupallam", "Chappathacode", "Devikulam", "Edamalakudy", 
+      "Erattayar", "Idukki", "Kanchiyar", "Kanjirappally", "Karimannur", 
+      "Kumily", "Mankulam", "Mariyapuram", "Munnar", "Nedumkandam", 
+      "Pallivasal", "Pampadumpara", "Peermade", "Purappuzha", "Rajakumari", 
+      "Santhanpara", "Senapathy", "Thodupuzha", "Udumbanchola", 
+      "Upputhara", "Vadimala", "Vazhathope", "Vathikudy"
+    ],
+    "municipalities": ["Thodupuzha", "Munnar"],
+    "corporations": []
+  },
+  
+  "Pathanamthitta": {
+    "panchayaths": [
+      "Anicadu", "Aruvappulam", "Cherukole", "Chittar", "Elacode", 
+      "Elanthoor", "Ezhamkulam", "Kadapra", "Kallooppara", "Kottangal", 
+      "Kozhencherry", "Kuttoor", "Mallappally", "Mallappuzhassery", 
+      "Mezhuveli", "Naranganam", "Naranamoozhi", "Niranam", "Omalloor", 
+      "Pallickal", "Pandalam", "Pathanamthitta", "Peringara", "Puramattom", 
+      "Ranni", "Seethathodu", "Thannithode", "Thiruvalla", "Thumpamon", 
+      "Vadasserikkara"
+    ],
+    "municipalities": ["Adoor", "Pandalam", "Pathanamthitta", "Thiruvalla"],
+    "corporations": []
+  },
+  
+  "Wayanad": {
+    "panchayaths": [
+      "Ambalavayal", "Bathery", "Edavaka", "Kalpetta", "Kaniyambetta", 
+      "Kaprassery", "Krishnagiri", "Kuppadi", "Mananthavady", "Meenangadi", 
+      "Mullankolly", "Muttil", "Nalloornad", "Noolpuzha", "Padinjarathara", 
+      "Panamaram", "Poothadi", "Pulpally", "Sulthan Bathery", "Thariyode", 
+      "Thirunelly", "Thondernad", "Tirunelli", "Vellamunda", "Vengapally"
+    ],
+    "municipalities": ["Kalpetta", "Mananthavady", "Sulthan Bathery"],
+    "corporations": []
+  }
+};
